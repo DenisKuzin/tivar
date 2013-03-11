d40b0b0d6d7c9644f1cd96b30808818ea150c2bc
DenisKuzin <digiholiq@gmail.com>
2013-03-11 17:05:58 +0400

    forts downloading

--- common.py
+++ common.py
@@ -2,8 +2,10 @@
 u'''
 Общие для всех модулей штуки
 '''
-from os.path import exists, isdir
-from os import remove, makedirs
+from os.path import exists, isdir, join
+from os import remove, makedirs, rename
+from settings import data_dir
+from requests import get
 
 def check_dir(path):
     u'''
@@ -16,3 +18,11 @@ def check_dir(path):
             makedirs(path)
     else:
         makedirs(path)
+
+def download_file(file_type, direct_link):
+    check_dir(join(data_dir, file_type))
+    file_path = join(data_dir, file_type, direct_link.split('/')[-1])
+    if not exists(file_path):
+        with open(u'%s.tmp' % file_path, 'wb') as current_file:
+            current_file.write(get(direct_link).content)
+        rename(u'%s.tmo', file_path)
--- retrieve/forts.py
+++ retrieve/forts.py
@@ -6,6 +6,9 @@ http://ftp.rts.ru/pub/info/stats/history/F/2003/FT030104.ZIP
 
 from datetime import datetime, date, time, timedelta
 from process.dates import work_dates_between
+from common import download_file
+from requests import get
+from lxml.html import fromstring
 
 forts_tradesfile_publish_time = time(19)
 u'''
@@ -13,11 +16,19 @@ u'''
 19:00
 '''
 
-def download_files():
+def parse_links():
+    for current_year in xrange(2003, date.today().year + 1):
+        page_link = u'http://ftp.rts.ru/pub/info/stats/history/F/%s/' % current_year
+        html = fromstring(get(page_link).text)
+        for current_link in html.make_links_absolute(page_link).iterlinks():
+            download_file(current_link)
+
+def generate_links():
     start_date = date(2003, 01, 04)
     if datetime.now().time() >= forts_tradesfile_publish_time:
         stop_date = date.today()
     else:
         stop_date = date.today() - timedelta(1)
     for current_date in work_dates_between(start_date, stop_date):
-        print current_date.strftime(u'http://ftp.rts.ru/pub/info/stats/history/F/%Y/FT%y%m%d.ZIP')
+        download_file('ft', current_date.strftime(u'http://ftp.rts.ru/pub/info/stats/history/F/%Y/FT%y%m%d.ZIP'))
+
